aa16a275c23e00d67507fa3bb444541f79ac8717
Robin van het Hof <robin.van.het.hof@gmail.com>
2020-10-09 15:15:10 +0200

    Update sync-bas-env.sh

---
+++ sync-bas-env.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+TARGET_DIR=~/.sync-bas-env
+VSCODE_PLUGINS_DIR=vscode-plugins
+
+helpfunction()
+{
+    echo ""
+    echo "Usage: $0 -o [ export | import ]"
+    echo -e "\t-o export Creates a tgz file with all relevant SAP BAS files"
+    echo -e "\t-o import After unpacking the tgz file it stores the content in the relevant folders, and installs nvm"
+    exit 1 # Exit script
+}
+
+while getopts "o:" opt
+do
+    case "$opt" in
+        o ) parameterO="$OPTARG" ;;
+        ? ) helpfunction ;; # Print help in case parameter unknown
+    esac
+done
+
+if [ -z "$parameterO" ]
+then
+    echo "Not all parameters are entered";
+    helpfunction
+fi
+
+if [ $parameterO == "export" ]
+then
+    mkdir -p $TARGET_DIR/.theia
+
+    cp -rf ~/.bashrc $TARGET_DIR
+    cp -rf ~/.profile $TARGET_DIR
+    [ -d ~/.scripts ] && cp -rf ~/.scripts $TARGET_DIR
+    [ -f ~/.gitconfig ] && cp -rf ~/.gitconfig $TARGET_DIR
+    [ -f ~/.git-credentials ] && cp -rf ~/.git-credentials $TARGET_DIR
+    [ -f ~/.theia/keymaps.json ] && cp -rf ~/.theia/keymaps.json $TARGET_DIR/.theia
+    [ -f ~/.theia/settings.json ] && cp -rf ~/.theia/settings.json $TARGET_DIR/.theia
+    [ -d ~/$VSCODE_PLUGINS_DIR ] && cp -rf ~/$VSCODE_PLUGINS_DIR $TARGET_DIR
+
+    cp -rf ~/sync-bas-env.sh $TARGET_DIR
+
+    tar cvfz sync-bas-env.tgz .sync-bas-env/
+
+    echo "Settings successfully exported"
+    echo ""
+elif [ $parameterO == "import" ]
+then
+    cp .bashrc ~
+    cp .profile ~
+    [ -d .scripts/ ] && cp -rf .scripts/ ~
+    [ -f .gitconfig ] && cp -rf .gitconfig ~
+    [ -f .git-credentials ] && cp -rf .git-credentials ~
+    [ -f .theia/keymaps.json ] && cp -rf .theia/keymaps.json ~/.theia/
+    [ -f .theia/settings.json ] && cp -rf .theia/settings.json ~/.theia/
+    [ -d $VSCODE_PLUGINS_DIR ] && cp -rf $VSCODE_PLUGINS_DIR ~/$VSCODE_PLUGINS_DIR
+
+    cp -rf sync-bas-env.sh ~
+
+    echo "Settings successfully imported into SAP BAS workspace"
+    echo ""
+    echo "NVM will now be installed..."
+    
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+    echo "Open a new terminal from SAP BAS, and execute:"
+    echo "$ nvm install 12"
+    echo ""
+    echo "To install the VSCode plugins: Ctrl-Shift-P > Deploy Plugin By Id > local-dir:/home/user/vscode-plugins/"
+    echo ""
+else
+    echo "Illegal argument for parameter -o"
+    helpfunction
+fi
